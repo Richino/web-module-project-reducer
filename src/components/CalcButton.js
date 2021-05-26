46deefd2e833c0b5acbcc5f6898b40c8ed586b70
Richino <archerrichino@gmail.com>
2021-05-25 23:22:34 -0400

    finished

--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const CalcButton = (props) => {
-    const { value, onClick = ()=>{}, size = 4} = props;
-
-    return(<div className={`col-xs-${size}`}>
-        <button type="button" onClick={onClick} className="btn">{value}</button>
-    </div>);
-}
+const CalcButton = ({ value, buttonClick = () => {}, size = 4 }) => {
+	return (
+		<div className={`col-xs-${size}`}>
+			<button type="button" onClick={buttonClick(value)} className="btn">
+				{value}
+			</button>
+		</div>
+	);
+};
 
 export default CalcButton;
